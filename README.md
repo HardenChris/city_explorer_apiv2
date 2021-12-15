0a66e180fa847eac627d836a9762a6d81b7068f8
Christopher Harden <Harden.Christopher00@gmail.com>
2021-12-14 22:33:02 -0600

    finished day one of api/server build

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# city_explorer_apiv2
+# City Explorer
+
+**Author**: Chris Harden
+**Version**: 1.0.1
+
+## Overview
+So far this application will help you find handy maps of locations you put in.
+
+## Getting Started
+To get started with this application you need to add your own values to an env, namely a locationIQ API key.
+
+## Architecture
+Just english language.
+
+## Change Log
+....will be updated later.
+
+## Credit and Collaborations
+Built myself using old code.
+
+Name of feature: ________________________________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(cors());
 
 
 
-app.get('/test', (req, res) => {res.status(200).send('Hello, I work and ready for lab!')});
-app.get('/hello', (req, res) => {res.status(200).send('Added a hello page, Yay!')});
+app.get('/test', (req, res) => { res.status(200).send('Hello, I work and ready for lab!') });
+app.get('/hello', (req, res) => { res.status(200).send('Added a hello page, Yay!') });
 app.get('/weather', handleGetWeather);
-
+app.get('/*', ((req, res) => res.status(404).send('unknown route')));
 
 
 const PORT = 3001;
@@ -22,23 +22,40 @@ const PORT = 3001;
 class Forecast {
     constructor(obj) {
         this.date = obj.datetime;
-        this.description = `a high of ${obj.max_temp}, a low of ${obj.min_temp}, with ${obj.weather.description}` 
+        this.description = `a high of ${obj.max_temp}, a low of ${obj.min_temp}, with ${obj.weather.description}`
     }
 }
 
-function handleGetWeather (req, res) {
-    const city_name = req.query.city_name;
-    const cityMatch = weatherData.find(city => city.city_name.toLowerCase() === city_name.toLowerCase());
-    if (cityMatch) {
-        let weatherDescription = cityMatch.data.map(day => new Forecast(day));
-        res.status(200).send(weatherDescription);
-    } else {
-        res.status(400).send('sorry no data for entered city');
-    }
+function handleGetWeather(req, res) {
+    console.log('handleGetWeather was hit')
+    const cityName = req.query.city;
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    // const cityMatch = weatherData.find((city => city.city_name.toLowerCase() === city_name.toLowerCase()) || );
+    try {
+        const cityToSend = weatherData.find(city => {
+            if ((city.lat === lat && city.lon === lon) || city.city_name === cityName) {
+                return true;
+            }
+            return false;
+        });
+        if (cityToSend) {
+            const forecastData = cityToSend.data.map(city => new Forecast(city));
+            console.log(forecastData)
+            res.status(200).send(cityToSend.data);
+        } else {
+            res.status(404).send('City is not found')
+        }
+    } catch (e) {
+        res.status(500).send('server error') }
+    // }
+    // if (cityMatch) {
+    //     let weatherDescription = cityMatch.data.map(day => new Forecast(day));
+    //     res.status(200).send(weatherDescription);
+    // } else {
+    //     res.status(400).send('sorry no data for entered city');
+    // }
 }
-
-
-
 
 
 
